13a48b91b36e8f544f98913c9e0623d48153621a
yakin1230 <ahmadyakin302512@gmail.com>
2025-02-19 00:38:26 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class name {
+    public static void main(String[] args) {
+        System.out.println("enter your name");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello "+ sc.nextLine());
+        
+    }
+    
+
+    
+}
